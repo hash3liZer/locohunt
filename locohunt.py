ff353350270c6c11c742434b47654ed82b758dc3
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-02-07 16:10:33 +0530

    Added Functionalities for enum and depth

--- locohunt.py
+++ locohunt.py
@@ -2,18 +2,34 @@ import argparse
 import sys
 import os
 import csv
+from pull import PULL
+from parser import PARSER
+
+class LOCOHUNT:
+
+	def __init__(self, prs):
+		self.tgtlist = prs.list
+		self.regexs  = prs.regexs
+		self.depth   = prs.depth
+
+	def engage(self):
+		return
 
 def main():
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('-t', '--target', dest="target", default="", type=str)
 	parser.add_argument('-r', '--regex', dest="regex", default="", type=str)
-	parser.add_argument('-f', '--regex-file', dest="regfile", default="", type=str)
+	parser.add_argument('-f', '--regex-json', dest="regfile", default="", type=str)
 	parser.add_argument('-d', '--depth', dest="depth", default=0, type=int)
 
 	opts = parser.parse_args()
-
 	parser = PARSER(opts)
+
+	print(parser.list)
+
+	#locohunt = LOCOHUNT(parser)
+	#locohunt.engage()
 
 if __name__ == "__main__":
 	main()
--- pull.py
+++ pull.py
@@ -1,5 +1,122 @@
+import sys
+import os
 
 class PULL:
 
+	WHITE = '\033[0m'
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
+	LINEUP = '\033[F'
+
+	MIXTURE = {
+		'WHITE': '\033[0m',
+		'PURPLE': '\033[95m',
+		'CYAN': '\033[96m',
+		'DARKCYAN': '\033[36m',
+		'BLUE': '\033[94m',
+		'GREEN': '\033[92m',
+		'YELLOW': '\033[93m',
+		'RED': '\033[91m',
+		'BOLD': '\033[1m',
+		'UNDERLINE': '\033[4m',
+		'END': '\033[0m',
+		'LINEUP': '\033[F'
+	}
+
+	VACANT = {
+		'WHITE': '',
+		'PURPLE': '',
+		'CYAN': '',
+		'DARKCYAN': '',
+		'BLUE': '',
+		'GREEN': '',
+		'YELLOW': '',
+		'RED': '',
+		'BOLD': '',
+		'UNDERLINE': '',
+		'END': '',
+		'LINEUP': ''
+	}
+
+
 	def __init__(self):
-		pass
+		if not self.support_colors:
+			self.win_colors()
+
+	def support_colors(self):
+		plat = sys.platform
+		supported_platform = plat != 'Pocket PC' and (plat != 'win32' or \
+														'ANSICON' in os.environ)
+		is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+		if not supported_platform or not is_a_tty:
+			return False
+		return True
+
+
+	def win_colors(self):
+		self.WHITE = ''
+		self.PURPLE = ''
+		self.CYAN = ''
+		self.DARKCYAN = ''
+		self.BLUE = ''
+		self.GREEN = ''
+		self.YELLOW = ''
+		self.RED = ''
+		self.BOLD = ''
+		self.UNDERLINE = ''
+		self.END = ''
+		self.MIXTURE = {
+			'WHITE': '',
+			'PURPLE': '',
+			'CYAN': '',
+			'DARKCYAN': '',
+			'BLUE': '',
+			'GREEN': '',
+			'YELLOW': '',
+			'RED': '',
+			'BOLD': '',
+			'UNDERLINE': '',
+			'END': '',
+			'LINEUP': ''
+		}
+
+		for (key, val) in self.MIXTURE.items():
+			self.MIXTURE[ key ] = ''
+
+	def run(self, string):
+		print(
+			"{colora}[']{colorb} {string}".format(
+				colora=self.DARKCYAN,
+				colorb=self.END,
+				string=string
+			)
+		)
+
+	def info(self, string):
+		print(
+			"{colora}[*]{colorb} {string}".format(
+				colora=self.YELLOW,
+				colorb=self.END,
+				string=string
+			)
+		)
+
+	def halt(self, string, exit=0):
+		print(
+			"{colora}[~]{colorb} {string}".format(
+				colora=self.RED,
+				colorb=self.END,
+				string=string
+			)
+		)
+
+		if exit:
+			sys.exit(exit)
